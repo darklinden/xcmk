ac60a444b681caec7927eb2ed1b172ff1de1d7b0
HanShaokun <darklinden@gmail.com>
2016-12-30 17:04:50 +0800

    update for new xcode

--- xcmk/main.mm
+++ xcmk/main.mm
@@ -13,11 +13,12 @@
 
 NSString *_prjPath = nil;
 
-NSString *_tagRegx = nil;
-NSString* _tagName = nil;
+NSString *_schRegx = nil;
+NSString* _schName = nil;
 
 NSString* _sigName = nil;
 NSString* _prfPath = nil;
+NSString *_prfName = nil;
 
 NSString* runCmd(NSString* cmd, NSArray* arguments, NSString* path = nil) {
     NSTask* task = [[NSTask alloc] init];
@@ -31,18 +32,28 @@ NSString* runCmd(NSString* cmd, NSArray* arguments, NSString* path = nil) {
     [task setStandardOutput:pout];
     
     [task launch];
+
+    NSMutableString* result = [NSMutableString string];
+
+    [[task.standardOutput fileHandleForReading] setReadabilityHandler:^(NSFileHandle *file) {
+        NSData *data = [file availableData]; // this will read to EOF, so call only once
+        NSString *tmp = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
+        printf("%s\n", tmp.UTF8String);
+
+        [result appendString:tmp];
+    }];
+
     [task waitUntilExit];
-    
-    NSFileHandle* read = [pout fileHandleForReading];
-    NSData* dataRead = [read readDataToEndOfFile];
-    return [[NSString alloc] initWithData:dataRead encoding:NSUTF8StringEncoding];
+
+    return result;
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         if (argc < 2) {
-            printf("\nxcmk\n -prj project path\n -tag target regex\n -sig CodeSign name\n -prf Provision Profile path\n\n");
+            printf("\nxcmk\n -prj project path\n -sch scheme regex\n -sig CodeSign name\n -prf Provision Profile path\n\n");
             return ErrorExit;
         }
         
@@ -53,9 +64,9 @@ int main(int argc, const char * argv[]) {
         if ([fmgr fileExistsAtPath:infoPath]) {
             NSDictionary* dict = [NSDictionary dictionaryWithContentsOfFile:infoPath];
             
-            NSString *tagRegx = dict[@"tag"];
+            NSString *tagRegx = dict[@"sch"];
             if (tagRegx && tagRegx.length) {
-                _tagRegx = tagRegx;
+                _schRegx = tagRegx;
             }
             
             NSString *sigName = dict[@"sig"];
@@ -104,9 +115,9 @@ int main(int argc, const char * argv[]) {
             _prjPath = projPath;
         }
         
-        NSString *tagRegx = [dict objectForKey:@"tag"];
-        if (tagRegx && tagRegx.length) {
-            _tagRegx = tagRegx;
+        NSString *schRegx = [dict objectForKey:@"sch"];
+        if (schRegx && schRegx.length) {
+            _schRegx = schRegx;
         }
         
         NSString *sigName = dict[@"sig"];
@@ -136,8 +147,8 @@ int main(int argc, const char * argv[]) {
         }
         
         //check tag
-        if (!(_tagRegx && _tagRegx.length)) {
-            printf("*** error: Project Target is incorrect! [%s] ***\n", _tagRegx.UTF8String);
+        if (!(_schRegx && _schRegx.length)) {
+            printf("*** error: Project Target is incorrect! [%s] ***\n", _schRegx.UTF8String);
             return ErrorExit;
         }
         
@@ -148,32 +159,29 @@ int main(int argc, const char * argv[]) {
         for (int i = 0; i < array.count; i++) {
             NSString* str = array[i];
             NSString* single = [str stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-            if ([single isEqualToString:@"Targets:"]) {
+            if ([single isEqualToString:@"Schemes:"]) {
                 isTag = true;
             }
-            else if ([single isEqualToString:@"Build Configurations:"]) {
-                isTag = false;
-            }
             else if (isTag) {
-                NSRegularExpression *regexv = [NSRegularExpression regularExpressionWithPattern:_tagRegx options:NSRegularExpressionCaseInsensitive error:nil];
+                NSRegularExpression *regexv = [NSRegularExpression regularExpressionWithPattern:_schRegx options:NSRegularExpressionCaseInsensitive error:nil];
                 
                 NSUInteger matchesCount = [regexv numberOfMatchesInString:single
                                                                   options:NSMatchingReportProgress
                                                                     range:NSMakeRange(0, single.length)];
                 
                 if (matchesCount) {
-                    _tagName = single;
+                    _schName = single;
                     break;
                 }
             }
         }
         
-        if (!(_tagName && _tagName.length)) {
-            printf("*** error: Get Project Target Failed! [%s] ***\n", _tagRegx.UTF8String);
+        if (!(_schName && _schName.length)) {
+            printf("*** error: Get Project Scheme Failed! [%s] ***\n", _schRegx.UTF8String);
             return ErrorExit;
         }
         else {
-            printf("Select Target [%s]\n", _tagName.UTF8String);
+            printf("Select Scheme [%s]\n", _schName.UTF8String);
         }
         
         //check sig
@@ -184,21 +192,7 @@ int main(int argc, const char * argv[]) {
         else {
             printf("CodeSign With [%s]\n", _sigName.UTF8String);
         }
-        
-        NSString* arguments = [NSString stringWithFormat:@"-target \"%@\" -configuration Release clean build \"CODE_SIGN_IDENTITY=%@\" DEPLOYMENT_POSTPROCESSING=YES", _tagName, _sigName];
-        
-        printf("building... \n");
-        
-        NSString* buildResult = runCmd(@"/usr/bin/xcodebuild", @[arguments], _prjPath);
-        
-        if ([buildResult rangeOfString:@"** BUILD SUCCEEDED **"].location == NSNotFound) {
-            printf("build failed:\n %s\n", buildResult.UTF8String);
-            return ErrorExit;
-        }
-        else {
-            printf("build success, exporting ipa... \n");
-        }
-        
+
         //check profile path
         pathExist = false;
         if (_prfPath && _prfPath.length) {
@@ -209,30 +203,78 @@ int main(int argc, const char * argv[]) {
                 }
             }
         }
-        
+
         if (!pathExist) {
             printf("*** error: Provision Profile Path is incorrect! [%s] ***\n", _prfPath.UTF8String);
             return ErrorExit;
         }
+
+        // get prf name
+        NSString *prfPlist = runCmd(@"/usr/bin/security", @[@"cms", @"-D", @"-i", _prfPath], _prjPath);
+        NSData *plistData = [prfPlist dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error;
+        NSPropertyListFormat format;
+        NSDictionary *plist = [NSPropertyListSerialization propertyListWithData:plistData
+                                                                        options:NSPropertyListImmutable
+                                                                         format:&format
+                                                                          error:&error];
+        if (!plist) {
+            NSLog(@"Error: %@", error);
+        }
+        else {
+//            printf("%s", plist.description.UTF8String);
+            _prfName = plist[@"Name"];
+            if (!_prfName) {
+                printf("*** error: Provision Profile Name is incorrect! [%s] ***\n", _prfPath.UTF8String);
+                return ErrorExit;
+            }
+        }
+
+
+        NSString *arcPath = [[_prjPath stringByAppendingPathComponent:@"build"] stringByAppendingPathComponent:@"arch.xcarchive"];
+
+#if 0
+        // arvhice
+        [fmgr createDirectoryAtPath:[_prjPath stringByAppendingPathComponent:@"build"] withIntermediateDirectories:YES attributes:nil error:nil];
+        [fmgr removeItemAtPath:arcPath error:nil];
         
+//        NSString* arguments = [NSString stringWithFormat:@"-target \"%@\" -configuration Release clean build \"CODE_SIGN_IDENTITY=%@\" DEPLOYMENT_POSTPROCESSING=YES", _schName, _sigName];
+
+        printf("building... \n");
+        
+        NSString* buildResult = runCmd(@"/usr/bin/xcodebuild", @[@"-scheme", _schName, @"clean", @"archive", @"-archivePath", arcPath], _prjPath);
+        
+        if ([buildResult rangeOfString:@"** ARCHIVE SUCCEEDED **"].location == NSNotFound) {
+            printf("build failed:\n %s\n", buildResult.UTF8String);
+            return ErrorExit;
+        }
+        else {
+            printf("build success, exporting ipa... \n");
+        }
+
+#endif
+
         //export ipa
-        NSString* app = [[[_prjPath stringByAppendingPathComponent:@"build/Release-iphoneos"] stringByAppendingPathComponent:_tagName]stringByAppendingPathExtension:@"app"];
-        NSString* ipa = [[[_prjPath stringByAppendingPathComponent:@"build/Release-iphoneos"] stringByAppendingPathComponent:_tagName]stringByAppendingPathExtension:@"ipa"];
+        NSString *ipaPath = [[_prjPath stringByAppendingPathComponent:@"build"] stringByAppendingPathComponent:@"build.ipa"];
+        [fmgr removeItemAtPath:ipaPath error:nil];
+
         NSMutableArray* arrayArguments = [NSMutableArray array];
-        [arrayArguments addObject:@"-sdk"];
-        [arrayArguments addObject:@"iphoneos"];
-        [arrayArguments addObject:@"PackageApplication"];
-//        [arrayArguments addObject:@"-v"];
-        [arrayArguments addObject:[NSString stringWithFormat:@"%@", app]];
-        [arrayArguments addObject:[NSString stringWithFormat:@"%@", ipa]];
-        [arrayArguments addObject:@"--embed"];
-        [arrayArguments addObject:[NSString stringWithFormat:@"%@", _prfPath]];
-        [arrayArguments addObject:@"--sign"];
-        [arrayArguments addObject:[NSString stringWithFormat:@"%@", _sigName]];
+        [arrayArguments addObject:@"-exportArchive"];
+        [arrayArguments addObject:@"-exportFormat"];
+        [arrayArguments addObject:@"ipa"];
+
+        [arrayArguments addObject:@"-archivePath"];
+        [arrayArguments addObject:arcPath];
+
+        [arrayArguments addObject:@"-exportPath"];
+        [arrayArguments addObject:ipaPath];
+
+        [arrayArguments addObject:@"-exportProvisioningProfile"];
+        [arrayArguments addObject:_prfName];
         
-        NSString* exportResult = runCmd(@"/usr/bin/xcrun", arrayArguments, _prjPath);
-        if ([exportResult rangeOfString:@"error"].location == NSNotFound) {
-            printf("ipa May Exported At [%s]\n", ipa.UTF8String);
+        NSString* exportResult = runCmd(@"/usr/bin/xcodebuild", arrayArguments, _prjPath);
+        if ([exportResult rangeOfString:@"** EXPORT FAILED **"].location == NSNotFound) {
+            printf("ipa May Exported At [%s]\n", ipaPath.UTF8String);
         }
         else {
             printf("Export Failed:\n%s\n", exportResult.UTF8String);
